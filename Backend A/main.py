457fc72961cdfb1c5dd1f409e39da7945483a3e5
Avinash <avinash1729d@gmail.com>
2026-07-31 01:12:53 +0530

    changed distance attributes

--- Backend A/main.py
+++ Backend A/main.py
@@ -83,7 +83,7 @@ def get_matches(request_id: int, db: Session = Depends(get_db)):
             request_id=request_id,
             hospital_id=c["hospital_id"],
             score=c["score"],
-            distance_km=c["distance_km"],
+            distance_km=c["distance_in_km"],
             status="proposed"
         )
         db.add(db_match)
