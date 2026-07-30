bfe19f567a82eb697b5b1013a3f801fa92ba4606
abhimathur1402 <abhishekmathur1402@gmail.com>
2026-07-30 22:36:11 +0530

    Add files via upload

---
+++ Backend B/Hospital - Matching/test_algorithm.py
@@ -0,0 +1,21 @@
+from algorithm import find_matches
+from data import hospitals
+
+request = {
+
+    "lat": 23.250,
+    "lng": 77.410,
+
+    "resource": "ICU",
+    "quantity": 2,
+
+    "blood_group": "A+"
+
+}
+
+result = find_matches(request, hospitals)
+
+print("Eligible Hospitals:\n")
+
+for hospital in result:
+    print(hospital)
